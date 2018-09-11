94ce8dbe816842bff4b92dc85a1aace6c618c9df
Sergii-Karpiak <sergii.karpiak@noobaa.com>
2018-09-11 12:48:37 +0300

    Change promises to async await

--- src/shared/services/Api.ts
+++ src/shared/services/Api.ts
@@ -1,10 +1,8 @@
 import "rxjs/add/observable/from";
-import { Observable } from "rxjs/Observable";
 
-const getWeather = (lat: number, lon: number) => {
-  const request = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=49f8541c5e9d0758175574596d1f532e`)
-    .then(response => response.json());
-  return Observable.from(request);
+const getWeather = async (lat: number, lon: number) => {
+  const data = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=49f8541c5e9d0758175574596d1f532e`)).json();
+  return data;
 };
 
 export {
