a163425059b004deb3c2d2121b8acb5d02bed872
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2018-12-07 10:42:55 +0900

    :up: Bump up

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
     path: __dirname + "/dist"
   },
 
+  mode: "development",
+
   // Enable sourcemaps for debugging webpack"s output.
   devtool: "source-map",
 
@@ -23,12 +25,4 @@ module.exports = {
     ]
   },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  }
 };
