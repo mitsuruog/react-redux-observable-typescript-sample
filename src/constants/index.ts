ae9b146fa78af57f5f0cd3ae94d1d7d6e7d7b3a1
Mitsuru Ogawa <mitsuru.ogawa.jp+github@gmail.com>
2019-02-12 22:39:52 +0900

    Update note

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,6 @@
+// NOTE
+// DO NOT USE dynamic string operations(like template string) as action type property.
+// see more details: https://github.com/piotrwitek/typesafe-actions#--the-actions
 export const MAP_READY   = "@@map/READY";
 export const WEATHER_GET = "@@weather/GET";
 export const WEATHER_SET = "@@weather/SET";
